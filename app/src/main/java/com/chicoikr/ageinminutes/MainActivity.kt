3bb81b5bdc59bd9f90641627fdbe8b88275f40bf
chicoikr <chicoikr@gmail.com>
2022-08-24 02:59:24 +0700

    Day Picker is displaying

--- app/src/main/java/com/chicoikr/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/chicoikr/ageinminutes/MainActivity.kt
@@ -1,10 +1,12 @@
 
 package com.chicoikr.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun asClicked(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+
+        DatePickerDialog(this,DatePickerDialog.OnDateSetListener{
+            view, year, month, dayofMonth -> },
+            year,
+            month,
+            day
+            ).show()
+
+
         Toast.makeText(this, "You clicked select", Toast.LENGTH_LONG).show()
     }
 }
